2689e25934bae2d1d067fef2de46fa03ec84087d
mtkache09 <apl5.tkachenko@gmail.com>
2025-07-01 23:39:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
+import asyncio
+
+bot = Bot(token="7993685695:AAHYUIY9YaI0oDK4OZKg0LpQgr65CWrpcBg")
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start_handler(message: types.Message):
+    keyboard = InlineKeyboardMarkup().add(
+        InlineKeyboardButton(
+            text="Открыть Mini App",
+            web_app=WebAppInfo(url="https://mtkache09.github.io/untitled3/")
+        )
+    )
+    await message.answer("Жми кнопку, чтобы открыть Mini App 👇", reply_markup=keyboard)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
+
