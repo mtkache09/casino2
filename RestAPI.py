5fd24d02dc7cfd0d2463042c284652a465c4e8bb
Vladimir <663vova@gmail.com>
2025-07-04 13:37:06 +0200

    Переделан под config

--- RestAPI.py
+++ RestAPI.py
@@ -3,13 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, HttpUrl
 from typing import List
 import asyncio
-import os
-from dotenv import load_dotenv
 import aiohttp
 from database import DatabaseManager
 from Cases import get_random_gift
-
-load_dotenv()
+from config import DATABASE_URL
 
 app = FastAPI()
 
@@ -21,13 +18,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-BOT_API_URL = str(os.getenv("BOT_API_URL"))
-if not BOT_API_URL:
-    raise ValueError("BOT_API_URL не найден в .env файле")
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL не найден в .env файле")
 
 db_manager = DatabaseManager(DATABASE_URL)
 
